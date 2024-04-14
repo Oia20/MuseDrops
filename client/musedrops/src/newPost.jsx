7f9e8182e2f914a02ce6558bf13bd199f5250125
Jacob <92279567+Oia20@users.noreply.github.com>
2024-04-14 13:22:25 -0500

    Creating post now requires being logged in.

--- client/musedrops/src/newPost.jsx
+++ client/musedrops/src/newPost.jsx
@@ -1,9 +1,27 @@
-// NewPost.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import "./tailindex.css"
+import { useNavigate, Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 function NewPost() {
+  const dispatch = useDispatch();
+  const loggedIn = useSelector(state => state.auth.loggedIn);
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (!loggedIn) {
+      navigate("/login")
+    }
+    return () => {
+      // Cleanup function
+      // This function runs when the component is unmounted or before it runs the effect again
+      // You can perform any cleanup tasks here, like clearing intervals, cancelling subscriptions, etc.
+    };
+  }, [/* dependency array */]); // Optional dependency array
+
   return (
-    <div>
+    <div className='h-screen'>
       <h2>New Post</h2>
       {/* Add your new post form or content here */}
     </div>
